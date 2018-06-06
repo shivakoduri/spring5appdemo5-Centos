123a1c39eda969697968eec0facb27532952bfc3
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-06-06 23:35:48 +0530

    initial project setup, removing unwanted files

--- src/main/java/com/myprojects/spring5/examples/applicationdemo/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myprojects.spring5.examples.applicationdemo.repositories;
-
-import com.myprojects.spring5.examples.applicationdemo.domain.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, String> {
-}
